9eb048776031f7938653daa31a37fc39b107e8f8
safwan <safwankamboh@gmail.com>
2026-01-11 16:57:40 +0500

    iniital cmt

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+// import prisma from "../config/prisma";
+import bcrypt from "bcryptjs";
+import prisma from "../config/prisma.js";
+
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Basic validation
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return res.status(201).json({ message: "Signup successful", userId: user.id });
+  } catch (error) {
+    console.error("SIGNUP ERROR:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
